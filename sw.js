7955e9cf551096f477a90bd8e369bc17a0506bea
Miikari <50832124+Miikari@users.noreply.github.com>
2026-04-02 16:41:52 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,11 @@
 const CACHE = 'tyotunnit-v1';
-const FILES = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
-
+const FILES = [
+  '/tyokirjanpito/',
+  '/tyokirjanpito/index.html',
+  '/tyokirjanpito/manifest.json',
+  '/tyokirjanpito/icons/icon-192.png',
+  '/tyokirjanpito/icons/icon-512.png'
+];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
@@ -15,6 +20,7 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/index.html')))
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/tyokirjanpito/index.html')
+))
   );
 });
